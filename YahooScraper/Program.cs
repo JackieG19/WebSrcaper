a7871edeb745f9aebfe8aafe74ea37f7ba2a7c38
Jackie <JGtheophila19@gmail.com>
2020-03-04 18:19:07 -0500

    successfully login but 2nd tab keeps popping up

--- YahooScraper/Program.cs
+++ YahooScraper/Program.cs
@@ -13,5 +13,19 @@ namespace YahooScraper
             options.AddArguments("--disable-gpu");
             var driver = new ChromeDriver(@"/Users/JackieMac/Projects/YahooScraper/YahooScraper/bin", options);
             driver.Navigate().GoToUrl("https://www.yahoo.com/");
+            
+            driver.FindElement(By.Id("header-signin-link")).Click();
+
+            var userName = driver.FindElement(By.Name("username"));
+            userName.SendKeys("throw-away-email");
+            userName.Submit();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            var pass = driver.FindElement(By.Id("login-passwd"));
+            pass.SendKeys("throw-away-password");
+            pass.Submit();
+            
+            // need to close 2nd tab pop-up
     }
 }
