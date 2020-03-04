c5b270f807ee1b507c9f86a22452a196b5795b07
Jackie <JGtheophila19@gmail.com>
2020-03-04 18:08:19 -0500

    successfully open to bowser

---
+++ YahooScraper/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+
+namespace YahooScraper
+{
+    class MainClass
+    {
+        var options = new ChromeOptions();
+        options.AddArguments("--disable-gpu");
+        var driver = new ChromeDriver(@"/Users/JackieMac/Projects/YahooScraper/YahooScraper/bin", options);
+        driver.Navigate().GoToUrl("https://www.yahoo.com/");
+    }
+}
