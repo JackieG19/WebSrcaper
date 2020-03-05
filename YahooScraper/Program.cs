22799e25ce1f81ecb0f4c895cc49442ecc7ca155
Jackie <JGtheophila19@gmail.com>
2020-03-05 11:06:02 -0500

    runs the scrape program

--- YahooScraper/Program.cs
+++ YahooScraper/Program.cs
@@ -1,7 +1,4 @@
 using System;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using System.Collections.Generic;
 
 namespace YahooScraper
 {
@@ -9,24 +6,12 @@ namespace YahooScraper
     {
         public static void Main(string[] args)
         {
-            var options = new ChromeOptions();
-            options.AddArguments("--disable-gpu");
-            var driver = new ChromeDriver(@"/Users/JackieMac/Projects/YahooScraper/YahooScraper/bin", options);
-            driver.Navigate().GoToUrl("https://www.yahoo.com/");
-            
-            driver.FindElement(By.Id("header-signin-link")).Click();
-
-            var userName = driver.FindElement(By.Name("username"));
-            userName.SendKeys("throw-away-email");
-            userName.Submit();
-
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-
-            var pass = driver.FindElement(By.Id("login-passwd"));
-            pass.SendKeys("throw-away-password");
-            pass.Submit();
-            
-            // need to close 2nd tab pop-up
+            var scrape = new scrape();
+            var results = scrape.scraper();
+            foreach (var stock in results)
+            {
+                Console.WriteLine(stock);
+            }
         }
     }
 }
